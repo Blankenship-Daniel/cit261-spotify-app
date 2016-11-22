ef78ebd560a7ec45ac4e0349e5b80c4f068566df
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-11-21 17:48:44 -0700

    added spa.js

---
+++ js/spa.js
@@ -0,0 +1,27 @@
+function reset_pages() {
+    var pages = document.getElementsByClassName('page');
+    for (var i = 0; i < pages.length; i++) {
+        if (pages[i].classList.contains('active')) {
+            pages[i].classList.remove('active');
+        }
+    }
+}
+
+function load_page(path) {
+    if (!window.location.hash) {
+        document.getElementById('home_page').classList.add('active');
+    } else {
+        var id = path.substr(1, path.length - 1);
+        document.getElementById(id).classList.add('active');
+    }
+}
+
+// Handles Single Page Application (SPA) logic
+window.addEventListener('load', function() {
+    load_page(window.location.hash);
+});
+
+window.addEventListener('hashchange', function() {
+    reset_pages();
+    load_page(window.location.hash);
+});
